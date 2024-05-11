0e5eadf22896ea94ba44809c34bc20a7767bd038
sohailahmed31 <160041648+sohailahmed31@users.noreply.github.com>
2024-05-11 10:42:39 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var isFriend = "Toheed";
+if (isFriend === "Toheed" || isFriend === "Abbas") {
+    console.log("".concat(isFriend, " is your friend."));
+}
+else {
+    console.log("".concat(isFriend, " is not your friend"));
+}
---
+++ main.ts
@@ -0,0 +1,6 @@
+let isFriend: string = "Toheed";
+if (isFriend === "Toheed" || isFriend === "Abbas") {
+  console.log(`${isFriend} is your friend.`);
+} else {
+  console.log(`${isFriend} is not your friend`);
+}
